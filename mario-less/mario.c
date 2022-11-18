5cdb275572d0d3c10aa98c15d9da782bf0797409
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-18 07:54:36 +0000

    Fri, Nov 18, 2022, 7:54 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,12 +3,9 @@
 
 int main(void)
 {
+    int n;
     do
     {
-        int n = get_int("Height: ");
-    }
-    while
-    {
-        n < 1 OR n > 8;
-    }
+        n = get_int("Height: ");
+    } while (n < 1 || n > 8);
 }
