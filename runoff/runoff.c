c3df3a06a3e852621e49f8bf3fe645767bc9fe07
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-19 19:19:25 +0000

    Sat, Nov 19, 2022, 7:19 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -145,7 +145,7 @@ void tabulate(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            int index = preference[i][j];
+            int index = preferences[i][j];
             if (!candidates[index].eliminated)
             {
                 candidates[index].votes++;
