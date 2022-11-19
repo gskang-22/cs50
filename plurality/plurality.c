dc41c3aca9fe202f2bd8030694037c5105722284
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-19 10:45:20 +0000

    Sat, Nov 19, 2022, 10:45 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -80,6 +80,10 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    
-    return;
+    int max = 0;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        int temp = candidates[i].votes;
+        max = max(temp, max);
+    }
 }
