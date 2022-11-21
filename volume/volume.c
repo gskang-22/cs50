c98a03e63957c457ef1a6e43e451bee868333ed6
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-21 17:49:48 +0000

    Mon, Nov 21, 2022, 5:49 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
     while (fread(&buffer, sizeof(int16_t), 1, input))
-    //fread returns the number of items of data successfully read
+        //fread returns the number of items of data successfully read
     {
         buffer *= factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
