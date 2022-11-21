38b9115615cd510db6ad7fbcb8f44239983ecf12
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-21 17:24:26 +0000

    Mon, Nov 21, 2022, 5:24 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -40,7 +40,8 @@ int main(int argc, char *argv[])
     fwrite(output, HEADER_SIZE, 1, &header);
 
     // TODO: Read samples from input file and write updated data to output file
-
+    int16_t buffer;
+    for (int i = 0, i < )
 
     // Close files
     fclose(input);
