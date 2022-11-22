fc5545895c55392672e99022aa47acae2801f7d6
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-22 12:23:23 +0000

    Tue, Nov 22, 2022, 12:23 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,15 +5,17 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./recover IMAGE");
+        printf("Usage: ./recover IMAGE\n");
         return 1;
     }
 
     file *F = fopen(argv[1], "r");
     if (F == NULL)
     {
-        prinf(")
+        prinf("Could not open %s.\n", argv[1]);
+        return 1;
     }
+    
     fread(###.jpg, 512, (sizeof(F)/512), F);
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
