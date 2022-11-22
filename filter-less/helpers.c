7552db8991b077e1509f6cba77ba143838371acc
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-22 11:56:12 +0000

    Tue, Nov 22, 2022, 11:56 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -90,7 +90,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             {
                 for (int l = -1; l < 2; l++)
                 {
-                    if (!( (i + k) < 0 || (j + l) < 0 || (i + k) > (height - 1) || (j + l) > (width - 1))
+                    if (!((i + k) < 0 || (j + l) < 0 || (i + k) > (height - 1) || (j + l) > (width - 1)))
                         {
                             totalr += image[i + k][j + l].rgbtRed;
                             totalg += image[i + k][j + l].rgbtGreen;
