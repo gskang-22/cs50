0346f4712b8f2ebd90343753ad9e428b39c88202
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-22 12:19:27 +0000

    Tue, Nov 22, 2022, 12:19 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,9 +5,11 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./recover %s", );
+        printf("Usage: ./recover IMAGE");
+        return 1;
     }
-    file *F = fopen(card.raw, "r");
+
+    file *F = fopen(argv[1], "r");
     fread(###.jpg, 512, (sizeof(F)/512), F);
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
