d9ece06248cf1cec0c87a9e4f747bbda2e8508d3
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-22 09:46:45 +0000

    Tue, Nov 22, 2022, 9:46 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,4 +1,5 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -7,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int x = (int)(image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0;
+            int x = round(image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0;
 
             image[i][j].rgbtRed = x;
             image[i][j].rgbtGreen = x;
@@ -27,31 +28,31 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int y = image[i][j].rgbtGreen;
             int z = image[i][j].rgbtBlue;
 
-            if (((int)(0.393 * x + 0.769 * y + 0.189 * z)) > 255)
+            if (round(0.393 * x + 0.769 * y + 0.189 * z) > 255)
             {
                 image[i][j].rgbtRed = 255;
             }
             else
             {
-                image[i][j].rgbtRed = ((int)(0.393 * x + 0.769 * y + 0.189 * z));
+                image[i][j].rgbtRed = round(0.393 * x + 0.769 * y + 0.189 * z);
             }
 
-            if (((int)(0.349 * x + 0.686 * y + 0.168 * z)) > 255)
+            if (round(0.349 * x + 0.686 * y + 0.168 * z) > 255)
             {
                 image[i][j].rgbtGreen = 255;
             }
             else
             {
-                image[i][j].rgbtGreen = ((int)(0.349 * x + 0.686 * y + 0.168 * z));
+                image[i][j].rgbtGreen = round(0.349 * x + 0.686 * y + 0.168 * z);
             }
 
-            if (((int)(0.272 * x + 0.534 * y + 0.131 * z)) > 255)
+            if (round(0.272 * x + 0.534 * y + 0.131 * z) > 255)
             {
                 image[i][j].rgbtBlue = 255;
             }
             else
             {
-                image[i][j].rgbtBlue = ((int)(0.272 * x + 0.534 * y + 0.131 * z));
+                image[i][j].rgbtBlue = round(0.272 * x + 0.534 * y + 0.131 * z) ;
             }
         }
     }
