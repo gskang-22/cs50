10742f9ae3231ea6b63cc6280ffce06b77c57790
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-23 07:28:37 +0000

    Wed, Nov 23, 2022, 7:28 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -20,9 +20,17 @@ int main(int argc, char *argv[])
     uint8_t buffer[BLOCK_SIZE];
     while (fread(buffer, 1, BLOCK_SIZE, F) == BLOCK_SIZE);
     {
+         // if jpg file
          if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-
+            fclose();
+            counter++;
+            fopen();
+            fwrite();
+        }
+        else
+        {
+            
         }
     }
 
