e93db9f6a886e1fec9c53636014b6c8c63a07615
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-24 08:31:02 +0000

    Thu, Nov 24, 2022, 8:31 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -28,7 +28,7 @@ bool check(const char *word)
 {
     // TODO
     int x = hash(word);
-    char *cursor[LENGTH + 1] = table[x]->next;
+    char **cursor = &(table[x]->next);
     while (cursor != NULL)
     {
         if (strcmp(word, cursor) == 0)
