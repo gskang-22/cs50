ee44492557f761961c67933bff0dbe8d4eac650d
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-24 07:32:24 +0000

    Thu, Nov 24, 2022, 7:32 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -39,6 +39,45 @@ bool load(const char *dictionary)
 {
     // TODO
     fopen(dictionary, "w");
+     while (fread(&c, sizeof(char), 1, file))
+    {
+        // Allow only alphabetical characters and apostrophes
+        if (isalpha(c))
+        {
+            // Append character to word
+            word[index] = c;
+            index++;
+
+            // Ignore alphabetical strings too long to be words
+            if (index > LENGTH)
+            {
+                // Consume remainder of alphabetical string
+                while (fread(&c, sizeof(char), 1, file) && isalpha(c));
+
+                // Prepare for new word
+                index = 0;
+            }
+        }
+
+        // Ignore words with numbers (like MS Word can)
+        else if (isdigit(c))
+        {
+            // Consume remainder of alphanumeric string
+            while (fread(&c, sizeof(char), 1, file) && isalnum(c));
+
+            // Prepare for new word
+            index = 0;
+        }
+
+        // We must have found a whole word
+        else if (index > 0)
+        {
+            // Terminate current word
+            word[index] = '\0';
+
+            // Update counter
+            words++;
+
     for
     {
         node *n = malloc(sizeof(node));
