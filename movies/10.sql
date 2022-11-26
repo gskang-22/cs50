56ed1a8924c9b926c610d895262f5ca2403ccd2c
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-26 18:43:51 +0000

    Sat, Nov 26, 2022, 6:43 PM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT name FROM people
-JOIN stars ON stars.person_id = people.id
-JOIN movies ON movies.id = stars.movie_id
-JOIN ratings ON rating.movie_id = movies.id
+SELECT DISTINCT name FROM directors
+JOIN people ON directors.person_id = people.id
+JOIN movies ON movies.id = directors.movie_id
+JOIN ratings ON ratings.movie_id = movies.id
 WHERE ratings.rating >= 9.0;
