c04e76497c279019924955ea500c6c96ed8375c8
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-26 18:24:16 +0000

    Sat, Nov 26, 2022, 6:24 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,1 +1,6 @@
 SELECT title FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON stars.person.id = people.id
+JOIN ratings ON ratings.movie_id = movies.id
+ORDER BY ratings.rating DESC
+LIMIT 5
