ff9cb1e80b3f099ab272d9ba6e00749b78c534ae
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-26 18:59:07 +0000

    Sat, Nov 26, 2022, 6:59 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,7 @@
 SELECT name FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
-WHERE 
+WHERE movies.title IN
+(SELECT title FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON people.id = stars.person_id)
