b894ec24a0d2e9f0ff5d49a9f49abdf6d71afc89
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-26 18:53:36 +0000

    Sat, Nov 26, 2022, 6:53 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,7 +2,7 @@ SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON people.id = stars.person_id
 WHERE people.name = "Johnny Depp"
-AND title =
+AND movies.title =
 (SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON people.id = stars.person_id
