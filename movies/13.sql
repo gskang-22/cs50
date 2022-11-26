aa4a57bd0e5d0e9ccdba6a6796138fbf3229c2e9
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-26 19:06:12 +0000

    Sat, Nov 26, 2022, 7:06 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -5,5 +5,5 @@ WHERE movies.id IN
 (SELECT movies.id FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON people.id = stars.person_id
-WHERE people.id = "Kevin Bacon" AND people.birth = 1958)
+WHERE people.name = "Kevin Bacon" AND people.birth = 1958)
 AND people.name != "Kevin Bacon";
