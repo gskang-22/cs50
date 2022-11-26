981cbd8d42ffa707638f9bb36cfe8ece0495e2fc
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-26 18:21:05 +0000

    Sat, Nov 26, 2022, 6:21 PM +00:00

---
+++ movies/10.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT name FROM people
+JOIN stars ON stars.person_id = person.id
+JOIN movies ON movies.id = stars.movie_id
+JOIN ratings ON rating.movie_id = movies.id
+WHERE ratings.rating >= 9.0
