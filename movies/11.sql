9a7802fea73765f6a9e83ea5635bd18c3b7fd173
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-26 18:34:37 +0000

    Sat, Nov 26, 2022, 6:34 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,6 +1,6 @@
 SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person.id = people.id
+JOIN people ON stars.person_id = people.id
 JOIN ratings ON ratings.movie_id = movies.id
 WHERE people.name = "Chadwick Boseman"
 ORDER BY ratings.rating DESC
