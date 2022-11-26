be32f24402f89d7eb11599d6024145fe20c10304
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-26 18:41:40 +0000

    Sat, Nov 26, 2022, 6:41 PM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -2,4 +2,4 @@ SELECT DISTINCT name FROM people
 JOIN stars ON stars.person_id = people.id
 JOIN movies ON movies.id = stars.movie_id
 JOIN ratings ON rating.movie_id = movies.id
-WHERE ratings.rating >= 9.0
+WHERE ratings.rating >= 9.0;
--- movies/12.sql
+++ movies/12.sql
@@ -6,4 +6,4 @@ AND movies.title =
 (SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON people.id = stars.person_id
-WHERE people.name = "Helena Bonham Carter")
+WHERE people.name = "Helena Bonham Carter");
