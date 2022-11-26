ea7c70ff84ea9fe20a06c0d24a671d045cafe3e4
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-26 18:36:38 +0000

    Sat, Nov 26, 2022, 6:36 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,9 @@
 SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON people.id = stars.person_id
-WHERE people.name = "Johnny Depp" AND "Helena Bonham Carter"
+WHERE people.name = "Johnny Depp"
+AND movies.title =
+(SELECT title FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON people.id = stars.person_id
+WHERE people.name = "Helena Bonham Carter")
