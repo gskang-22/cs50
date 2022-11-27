375a9723acb499c5661998ef781a46a571b80fed
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-27 18:56:20 +0000

    Sun, Nov 27, 2022, 6:56 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,17 +1,15 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-
 -- took place on July 28, 2021 and that it took place on Humphrey Street.
---
 
---finding who the thief is
+--case description
 SELECT description
 FROM crime_scene_reports
 WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
-
+--getting transcript of witnesses
 SELECT transcript
 FROM interviews
 WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
-
+--witness1
 SELECT people.name, bakery_security_logs.license_plate, bakery_security_logs.activity
 FROM bakery_security_logs
 JOIN people ON bakery_security_logs.license_plate = people.license_plate
@@ -20,7 +18,7 @@ AND bakery_security_logs.month = 7
 AND bakery_security_logs.day = 28
 AND bakery_security_logs.hour = 10
 AND (bakery_security_logs.minute >= 15 AND bakery_security_logs.minute <= 25);
-
+--witness2
 SELECT people.name
 FROM people
 JOIN bank_accounts ON bank_accounts.person_id = people.id
@@ -30,3 +28,8 @@ AND atm_transactions.month = 7
 AND atm_transactions.day = 28
 AND atm_transactions.atm_location = "Leggett Street"
 AND atm_transactions.transaction_type = "withdraw";
+--witness3
+SELECT caller, receiver
+FROM phone_calls
+WHERE year = 2021 AND month = 7 AND day = 28
+AND duration < 60;
