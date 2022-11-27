84bc1e6ec355c54f5cdf231221409ca3f6a4f319
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-27 19:51:11 +0000

    Sun, Nov 27, 2022, 7:51 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -72,7 +72,9 @@ AND bakery_security_logs.month = 7
 AND bakery_security_logs.day = 28
 AND bakery_security_logs.hour = 10
 AND (bakery_security_logs.minute >= 15 AND bakery_security_logs.minute <= 25)
-AND name IN (SELECT people.name
+
+AND name IN
+(SELECT people.name
 FROM people
 JOIN bank_accounts ON bank_accounts.person_id = people.id
 JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
@@ -81,22 +83,13 @@ AND atm_transactions.month = 7
 AND atm_transactions.day = 28
 AND atm_transactions.atm_location = "Leggett Street"
 AND atm_transactions.transaction_type = "withdraw")
-AND name IN (SELECT name FROM people
+
+AND name IN
+(SELECT DISTINCT name FROM people
 JOIN phone_calls ON people.phone_number = phone_calls.caller
-WHERE people.phone_number = (SELECT caller
+WHERE people.phone_number IN (SELECT caller
 FROM phone_calls
 WHERE year = 2021 AND month = 7 AND day = 28
 AND duration < 60))
-AND name IN (SELECT people.name
-FROM people
-JOIN passengers ON passengers.passport_number = people.passport_number
-JOIN flights ON passengers.flight_id = flights.id
-WHERE flights.id =
-(SELECT flights.id
-FROM flights
-JOIN airports ON flights.destination_airport_id = airports.id
-WHERE flights.year = 2021 AND flights.month = 7 AND flights.day = 29
-AND flights.origin_airport_id =
-(SELECT id FROM airports WHERE city = "Fiftyville")
-ORDER BY hour ASC, minute ASC
-LIMIT 1));
+
+AND name IN
