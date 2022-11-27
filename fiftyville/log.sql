29535b441541932625e1d6936ba6d5a9749081a2
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-27 18:22:52 +0000

    Sun, Nov 27, 2022, 6:22 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,4 +6,4 @@
 --finding who the thief is
 SELECT description
 FROM crime_scene_reports
-WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Chamberlin Street";
+WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
