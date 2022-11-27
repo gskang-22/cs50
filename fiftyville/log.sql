401a4cb4f2ba8e11699b66d082b7633366098aa6
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-11-27 18:05:16 +0000

    Sun, Nov 27, 2022, 6:05 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- took place on July 28, 2021 and that it took place on Humphrey Street.
+-- 
+
+--finding who the thief is
+SELECT
