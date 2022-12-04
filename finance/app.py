c1df4439cba1f27f7cb14d6112b2a478b0c2858f
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-04 22:32:53 +0000

    Sun, Dec 4, 2022, 10:32 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -72,7 +72,7 @@ def buy():
 
     if request.method == "POST":
         get_quote = lookup(request.form.get("symbol"))
-        shares_number = int(request.form.get("shares"))
+        shares_number = int(float(request.form.get("shares")))
 
         if not request.form.get("symbol"):
             return apology("input symbol is blank", 400)
