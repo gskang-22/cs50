5d4792b52064d1adf304bd36b48011fcaa3f6637
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-04 17:51:50 +0000

    Sun, Dec 4, 2022, 5:51 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -45,7 +45,7 @@ def after_request(response):
 def index():
     """Show portfolio of stocks"""
     rows = db.execute("SELECT symbol, SUM(shares_number) FROM users_history WHERE user_id = ?", session["user_id"])
-    cash = db.execute("SELECT cash FROM users WHERE id = ?", session ["id"])
+    cash[0]["cash"] = db.execute("SELECT cash FROM users WHERE id = ?", session ["id"])
     investment_total = 0
     for row in rows:
         stock_quote = lookup(symbol)
