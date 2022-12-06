60f1f2595e3adb7eadefc0d745b78b2920865617
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-06 16:33:20 +0000

    Tue, Dec 6, 2022, 4:33 PM +00:00

---
+++ python/playback/playback.py
@@ -0,0 +1,7 @@
+x = input("")
+if x == "This is CS50":
+    print("This...is...CS50")
+elif x == "This is our week on functions":
+    print("This...is...our...week...on...functions")
+elif x == "Let's implement a function called hello":
+    print("Let's...implement...a...function...called...hello")
