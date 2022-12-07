5830bbe4e3ddcbf977076a8df9a3937125624c03
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-07 18:09:34 +0000

    Wed, Dec 7, 2022, 6:09 PM +00:00

--- python/deep/deep.py
+++ python/deep/deep.py
@@ -1,5 +1,5 @@
 x = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
-x = x.lower()
+x = x.strip().lower()
 
 if x == "42" or  x == "forty-two":
     print("Yes")
