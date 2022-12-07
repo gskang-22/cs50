0191fa84c3b5a4315cd22627ff5ae176a068e89f
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-07 18:32:59 +0000

    Wed, Dec 7, 2022, 6:32 PM +00:00

---
+++ python/extensions/extensions.py
@@ -0,0 +1,13 @@
+x = input("File name: ").lower()
+if ".gif"in x:
+    print("image/gif")
+elif ".jpg"in x:
+    print("image/jpg")
+elif ".jpeg"in x:
+    print("image/jpeg")
+elif ".png"in x:
+    print("image/png")
+elif ".pdf"in x:
+    print("application/pdf")
+elif ".txt"in x:
+    print("image/jpg")
