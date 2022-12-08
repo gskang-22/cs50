8c01a3291ebcfe3260b8e70822728cc2c1e8e786
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-08 18:27:58 +0000

    Thu, Dec 8, 2022, 6:27 PM +00:00

--- python/plates/plates.py
+++ python/plates/plates.py
@@ -8,3 +8,11 @@ def main():
 
 def is_valid(s):
     count_letters = 0
+    if not s[0:1].isalpha():
+        return False
+    elif len(s) > 6 or len(s) < 2:
+        return False
+    for char in s:
+        if not char.isalpha() or not char.isdigit():
+            return False
+        
