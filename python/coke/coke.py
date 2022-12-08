78fb7a3a186d6f749aae2e44f0c4864a98de1b88
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-08 10:02:38 +0000

    Thu, Dec 8, 2022, 10:02 AM +00:00

--- python/coke/coke.py
+++ python/coke/coke.py
@@ -5,8 +5,9 @@ while cost > 0:
     coin = input("Insert Coin: ")
     if coin == "25" or coin == "10" or coin == "5":
         cost -= int(coin)
-        print(f"Amount Due: {cost}")
+        if cost > 0:
+            print(f"Amount Due: {cost}")
+        elif cost <= 0:
+            print(f"Change owed: {-cost}")
     else:
         print(f"Amount Due: {cost}")
-
-print(f"Change owed: {-cost}")
