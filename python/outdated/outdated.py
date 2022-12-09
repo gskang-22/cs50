6d82a237299c1b474a48f4072f54ff75e84954f1
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-09 19:14:48 +0000

    Fri, Dec 9, 2022, 7:14 PM +00:00

---
+++ python/outdated/outdated.py
@@ -0,0 +1,14 @@
+year = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
