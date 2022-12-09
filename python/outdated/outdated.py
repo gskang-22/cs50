bb02cb570be73bb4657687ffbdc502705e5154ff
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-09 19:22:48 +0000

    Fri, Dec 9, 2022, 7:22 PM +00:00

--- python/outdated/outdated.py
+++ python/outdated/outdated.py
@@ -1,4 +1,4 @@
-year = [
+list = [
     "January",
     "February",
     "March",
@@ -12,3 +12,23 @@ year = [
     "November",
     "December"
 ]
+
+while True:
+    date = input("Date: ")
+
+    if date[0].isdigit():
+        month, day, year = date.split("/")
+        if month > 12 or day > 31:
+            continue
+
+        print(f"{year}-{month:02}-{day:02}")
+        break
+
+    else:
+        month, day, year = date.split(" ")
+        if month not in list:
+            continue
+
+        for c in day:
+            if not c.isdigit():
+                
