d5d12e3bb58e06b9089c8507a259aabba1d7707f
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-09 19:28:57 +0000

    Fri, Dec 9, 2022, 7:28 PM +00:00

--- python/outdated/outdated.py
+++ python/outdated/outdated.py
@@ -18,7 +18,7 @@ while True:
 
     if date[0].isdigit():
         month, day, year = date.split("/")
-        if month > 12 or day > 31:
+        if int(month) > 12 or int(day) > 31:
             continue
 
         print(f"{year}-{month:02}-{day:02}")
@@ -29,6 +29,19 @@ while True:
         if month not in list:
             continue
 
+        day_act = ""
         for c in day:
-            if not c.isdigit():
-                
+            if c.isdigit():
+                day_act += c
+
+        if int(day_act) > 31:
+            continue
+
+        print(f"{year}-{get_month(month):02}-{day:02})
+        break
+
+
+def get_month(month):
+    for i in range(len(list)):
+            if month == list[i]
+                return i
