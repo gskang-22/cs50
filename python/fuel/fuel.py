85f9ebdabcf1f15cb938f52929e0761536bca9e2
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-09 15:51:59 +0000

    Fri, Dec 9, 2022, 3:51 PM +00:00

--- python/fuel/fuel.py
+++ python/fuel/fuel.py
@@ -1,17 +1,22 @@
-while True
+while True:
     a = input("Fraction: ")
     x, y = a.split("/")
 
-    if not x.isdigit() or not y.isdigit():
-        continue
-
-    x = float(x)
-    y = float(y)
-    if x > y or y = 0:
-        continue
 
     try:
-        fraction = (x / y) * 100
-        print(f"{int(fraction)}%")
-    except:
-        
+        x = float(x)
+        y = float(y)
+
+        if x > y or y == 0:
+            continue
+
+        fraction = round((x / y), 2) * 100
+        if fraction > 99:
+            print("F")
+        else:
+            print(f"{int(fraction)}%")
+    except ZeroDivisionError:
+        print("E")
+
+    except ValueError:
+        continue
