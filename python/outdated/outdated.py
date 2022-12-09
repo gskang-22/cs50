e310a56e72040041f9c79d547347885aeea74b54
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-09 19:40:52 +0000

    Fri, Dec 9, 2022, 7:40 PM +00:00

--- python/outdated/outdated.py
+++ python/outdated/outdated.py
@@ -12,37 +12,39 @@ list = [
     "November",
     "December"
 ]
-get_month()
+def main():
+    while True:
+        date = input("Date: ")
 
-while True:
-    date = input("Date: ")
+        if date[0].isdigit():
+            month, day, year = date.split("/")
+            if int(month) > 12 or int(day) > 31:
+                continue
 
-    if date[0].isdigit():
-        month, day, year = date.split("/")
-        if int(month) > 12 or int(day) > 31:
-            continue
+            print(f"{year}-{int(month):02}-{int(day):02}")
+            break
 
-        print(f"{year}-{int(month):02}-{int(day):02}")
-        break
+        else:
+            month, day, year = date.split(" ")
+            if month not in list:
+                continue
 
-    else:
-        month, day, year = date.split(" ")
-        if month not in list:
-            continue
+            day_act = ""
+            for c in day:
+                if c.isdigit():
+                    day_act += c
 
-        day_act = ""
-        for c in day:
-            if c.isdigit():
-                day_act += c
+            if int(day_act) > 31:
+                continue
 
-        if int(day_act) > 31:
-            continue
-
-        print(f"{year}-{get_month(month):02}-{day:02}")
-        break
+            print(f"{year}-{get_month(month):02}-{int(day_act):02}")
+            break
 
 
 def get_month(month):
     for i in range(len(list)):
             if month == list[i]:
-                return i
+                return i + 1
+
+if __name__ == "__main__":
+    main()
