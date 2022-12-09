87d2dc49a013af0bfe3c1a2d03854e7c97e0905f
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-09 19:06:52 +0000

    Fri, Dec 9, 2022, 7:06 PM +00:00

--- python/grocery/grocery.py
+++ python/grocery/grocery.py
@@ -1,2 +1,16 @@
-dict = 
-}
+dict = {}
+
+while True:
+    try:
+        item = input("").upper
+
+        if item in dict:
+            dict[item] += 1
+        else:
+            dict[item] = 1
+
+    except EOFError:
+        for key in dict:
+            print(f"{dict[key]} {key}")
+
+        break
