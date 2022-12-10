e12fa3a87d7c81e993315f2d686ed663b83ca844
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-10 13:54:14 +0000

    Sat, Dec 10, 2022, 1:54 PM +00:00

--- python/figlet/figlet.py
+++ python/figlet/figlet.py
@@ -3,4 +3,11 @@ import sys
 
 figlet = Figlet()
 list = figlet.getFonts()
-print(list)
+
+if len(sys.argv) != 3:
+    sys.exit("Invalid Usage")
+elif sys.argv[1] != "-f" or sys.argv[1] != "--font":
+    sys.exit("Invalid Usage")
+
+x = input("Input: ")
+print(figlet.renderText(x))
