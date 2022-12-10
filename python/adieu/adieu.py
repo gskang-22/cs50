6365e2fe3397cd6b45a1cb7c5a0f99446e0f992b
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-10 14:14:24 +0000

    Sat, Dec 10, 2022, 2:14 PM +00:00

--- python/adieu/adieu.py
+++ python/adieu/adieu.py
@@ -1,7 +1,13 @@
+import inflect
+p = inflect.engine()
+list = []
+
 while True:
     try:
         x = input("Name: ")
+        list.append(x)
+
+    except EOFError:
         
         print(f"Adieu, adieu, to {}")
-    except EOFError:
         break
