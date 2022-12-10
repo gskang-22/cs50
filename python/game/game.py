7893cb050ae20eeaf6b368b2e70589e7ac5fed56
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-10 16:54:08 +0000

    Sat, Dec 10, 2022, 4:54 PM +00:00

--- python/game/game.py
+++ python/game/game.py
@@ -2,9 +2,17 @@ import random
 
 while True:
     level = input("Level: ")
-    if not level .isdigit():
+    if not level.isdigit():
         continue
     if int(level) <= 0:
         continue
 
-x = random.random(1, int(level))
+    x = random.random(1, int(level))
+
+    guess = input("Guess: ")
+    if not guess..isdigit():
+        continue
+    if int(guess) <= 0:
+        continue
+
+    
