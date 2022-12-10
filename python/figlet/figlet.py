48156063ed34fbbb7516ef5193c6d1c90ccb78ff
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-10 14:00:05 +0000

    Sat, Dec 10, 2022, 2:00 PM +00:00

--- python/figlet/figlet.py
+++ python/figlet/figlet.py
@@ -5,13 +5,13 @@ figlet = Figlet()
 list = figlet.getFonts()
 
 if len(sys.argv) != 3:
-    sys.exit("Invalid Usage")
-elif sys.argv[1] != "-f" or sys.argv[1] != "--font":
-    sys.exit("Invalid Usage")
+    sys.exit("Invalid Usage 1")
+elif sys.argv[1].strip() != "-f" and sys.argv[1] != "--font":
+    sys.exit("Invalid Usage 2")
 
 f = sys.argv[2]
 if f not in list:
-    sys.exit("Invalid Usage")
+    sys.exit("Invalid Usage 3")
 else:
     figlet.setFont(font=f)
 
