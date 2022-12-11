57b5407b025842fae6ecf6037215c063919b459b
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-11 19:14:07 +0000

    Sun, Dec 11, 2022, 7:14 PM +00:00

--- python/test_bank/bank.py
+++ python/test_bank/bank.py
@@ -1,9 +1,18 @@
 def main():
-    ...
+    x = input("Greeting: ")
+    value = value(x)
+    print(value)
 
 
 def value(greeting):
-    ...
+    greeting = greeting.lower()
+
+    if "hello" in greeting:
+        return "$0"
+    elif greeting[0] == "h":
+        return "$20"
+    else:
+        return "$100"
 
 
 if __name__ == "__main__":
