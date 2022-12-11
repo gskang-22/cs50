2259b81ec21b437a409dba20b9236c5eefdc5da3
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-11 19:27:05 +0000

    Sun, Dec 11, 2022, 7:27 PM +00:00

--- python/test_bank/test_bank.py
+++ python/test_bank/test_bank.py
@@ -2,9 +2,12 @@ from bank import value
 
 def test_0():
     assert value("hello there") == 0
+    assert value("HELLO THERE") == 0
 
 def test_20():
     assert value("how are you?") == 20
+    assert value("HOW ARE YOU?") == 20
 
 def test_100():
     assert value("asdf") == 100
+    assert value("ASDF") == 100
