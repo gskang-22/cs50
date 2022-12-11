115fc90f1cea6da91c968b5457aea05991ede970
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-11 19:40:05 +0000

    Sun, Dec 11, 2022, 7:40 PM +00:00

--- python/test_plates/test_plates.py
+++ python/test_plates/test_plates.py
@@ -1,9 +1,16 @@
 from plates import is_valid
 
 def test_not0():
-    assert is_valid("CS05") == True
+    assert is_valid("CS05") == False
 
 def test_not_middle():
     assert is_valid("CS50P") == False
 
-def test_
+def test_no_numbers():
+    assert is_valid("OUTATIME") == False
+
+def test_no_special():
+    assert is_valid("PI3.14") == False
+
+def test_true():
+    assert is_valid("CS50") == True
