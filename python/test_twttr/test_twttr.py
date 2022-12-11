c70be5b1d02bb6a88e05c58e876211303af7707f
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-11 18:47:41 +0000

    Sun, Dec 11, 2022, 6:47 PM +00:00

--- python/test_twttr/test_twttr.py
+++ python/test_twttr/test_twttr.py
@@ -3,3 +3,4 @@ from twttr import shorten
 def test_twttr():
     assert shorten("hello") == "hll"
     assert shorten("testing this test") == "tstng ths tst"
+    assert shorten("1234?") == "12324?"
