a8aeee2ce560c5bd697b6052866a0cd37a5ee37d
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-11 17:39:49 +0000

    Sun, Dec 11, 2022, 5:39 PM +00:00

--- python/test_twttr/test_twttr.py
+++ python/test_twttr/test_twttr.py
@@ -1,9 +1,12 @@
 def main():
-    ...
+    
 
 
 def shorten(word):
-    ...
+    x = ""
+    for char in word:
+        if char != "a" and char != "e" and char != "i" and char != "o" and char != "u":
+            x += char
 
 
 if __name__ == "__main__":
