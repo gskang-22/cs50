80d709639d440725b38c1d5001c21ff0fb28e1a1
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-11 19:32:19 +0000

    Sun, Dec 11, 2022, 7:32 PM +00:00

--- python/test_plates/plates.py
+++ python/test_plates/plates.py
@@ -1,9 +1,30 @@
 def main():
-    ...
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
 
 def is_valid(s):
-    ...
+    if not s[0:1].isalpha():
+        return False
+
+    elif len(s) > 6 or len(s) < 2:
+        return False
+
+    for char in s:
+        if not char.isalpha() and not char.isdigit():
+            return False
+
+    for i in range(len(s)):
+        if s[i].isdigit():
+            if s[i] == "0" or not s[i:len(s)].isdigit():
+                return False
+            else:
+                break
+
+    return True
 
 
 if __name__ == "__main__":
