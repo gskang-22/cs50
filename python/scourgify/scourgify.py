a5fa55fcac0bb2992107da28e883b0c492591987
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-12 20:17:23 +0000

    Mon, Dec 12, 2022, 8:17 PM +00:00

--- python/scourgify/scourgify.py
+++ python/scourgify/scourgify.py
@@ -2,22 +2,31 @@ import csv
 import sys
 
 def main():
+    list = []
     try:
         if len(sys.argv) > 3:
             sys.exit("Too few command-line arguments")
         elif len(sys.argv) < 3:
             sys.exit("Too many command-line arguments")
 
-        with open(sys.argv[1], "r") as file1, open(sys.argv[2], "w") as file2:
+        with open(sys.argv[1], "r") as file1:
             csv1 = csv.reader(file1)
             for row in csv1:
                 full_name = row[0]
+                house = row[1]
                 if full_name == "name":
                     continue
 
                 temp = []
-                last, name = full_name.split(",")
-                
+                last, first = full_name.split(",")
+
+                temp.append(first.strip())
+                temp.append(last.strip())
+                temp.append(house)
+                list.append(temp)
+
+        with open(sys.argv[2], "w") as file2:
+            
 
 
     except FileNotFoundError:
