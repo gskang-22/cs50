c36b93e9971b9c77c7168a1ac48c4f5316e47950
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-12 17:35:37 +0000

    Mon, Dec 12, 2022, 5:35 PM +00:00

--- python/bitcoin/bitcoin.py
+++ python/bitcoin/bitcoin.py
@@ -1,10 +1,16 @@
-def main():
-    import requests
+import requests
+import sys
 
+def main():
     try:
-        ...
+        if len(sys.argv) < 2:
+            print("Missing command-line argument")
+        n = float(sys.argv[1])
+
     except requests.RequestException:
         ...
+    except ValueError:
+        sys.exit("Command-line argument is not a number")
 
 if __name__ == "__main__":
     main()
