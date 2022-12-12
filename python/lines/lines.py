e346ac79b0d43a643b8ed31c8a9607c7fb985462
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-12 17:07:11 +0000

    Mon, Dec 12, 2022, 5:07 PM +00:00

--- python/lines/lines.py
+++ python/lines/lines.py
@@ -2,11 +2,11 @@ import sys
 
 def main():
     if len(sys.argv) < 2:
-        print("Too few command-line arguments")
+        sys.exit("Too few command-line arguments")
     elif len(sys.argv) > 2
-        print("Too many command-line arguments")
+        sys.exit("Too many command-line arguments")
 
-    
+
 
 
 if __name__ == "__main__":
