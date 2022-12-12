86f22655be8f3b35c41ea6370e805ced79ac1c61
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-12 19:59:44 +0000

    Mon, Dec 12, 2022, 7:59 PM +00:00

--- python/scourgify/scourgify.py
+++ python/scourgify/scourgify.py
@@ -2,13 +2,18 @@ import csv
 import sys
 
 def main():
-    if len(sys.argv) > 3:
-        sys.exit("Too few command-line arguments")
-    elif len(sys.argv) < 3:
-        sys.exit("Too many command-line arguments")
+    try:
+        if len(sys.argv) > 3:
+            sys.exit("Too few command-line arguments")
+        elif len(sys.argv) < 3:
+            sys.exit("Too many command-line arguments")
 
-    with open(sys.argv[1], "r") as file1, open(sys.argv[2], "w") as file2:
+        with open(sys.argv[1], "r") as file1, open(sys.argv[2], "w") as file2:
+            for row in file1:
+                
 
+    except FileNotFoundError:
+        sys.exit(f"could not read{argv[1]}")
 
 if __name__ == "__main__":
     main()
