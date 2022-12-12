aa5af8bc276d3e51c7124cc97e8e12ce2ee9947c
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-12 17:45:36 +0000

    Mon, Dec 12, 2022, 5:45 PM +00:00

--- python/bitcoin/bitcoin.py
+++ python/bitcoin/bitcoin.py
@@ -4,7 +4,7 @@ import sys
 def main():
     try:
         if len(sys.argv) < 2:
-            print("Missing command-line argument")
+            sys.exit("Missing command-line argument")
         n = float(sys.argv[1])
         r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
         o = r.json()
