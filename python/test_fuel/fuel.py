1e38b94e0ae2b555fcf621a8e0b42cb1525dd52e
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-12 09:43:14 +0000

    Mon, Dec 12, 2022, 9:43 AM +00:00

--- python/test_fuel/fuel.py
+++ python/test_fuel/fuel.py
@@ -1,13 +1,41 @@
 def main():
-    ...
+    while True:
+        x = input("Fraction: ")
+        try:
+            y = convert(x)
+            z = gauge(y)
+            print(z)
+            break
+
+        except ZeroDivisionError:
+            continue
+        except ValueError:
+            continue
+
 
 
 def convert(fraction):
-    ...
+    x, y = fraction.split("/")
+    if not x.isdigit() or not y.isdigit():
+        raise ValueError
+
+    x = float(x.strip())
+    y = float(y.strip())
+    if x > y:
+        raise ValueError
+    elif y == 0:
+        raise ZeroDivisionError
+
+    return round((x / y) * 100)
 
 
 def gauge(percentage):
-    ...
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
+    else:
+        return f"{percentage}%"
 
 
 if __name__ == "__main__":
