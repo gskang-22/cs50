6135c8cd235158e8b41d9df7fe36082df10983b8
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-12 17:05:22 +0000

    Mon, Dec 12, 2022, 5:05 PM +00:00

--- python/lines/lines.py
+++ python/lines/lines.py
@@ -1,6 +1,11 @@
 import sys
 
 def main():
+    if len(sys.argv) < 2:
+        print("Too few command-line arguments")
+    elif len(sys.argv) > 2
+        print("Too many command-line arguments")
+
     
 
 
