1cdfaf8cece0be2d313a47c98c902da5bdce2e6c
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-13 09:52:30 +0000

    Tue, Dec 13, 2022, 9:52 AM +00:00

---
+++ python/shirt/shirt.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+    try:
+        if len(sys.argv) > 2:
+            sys.exit("Too many command-line arguments")
+        elif len(sys.argv) < 2:
+            sys.exit("Too few command-line arguments")
+
+if __name__ == "__main__":
+    main()
