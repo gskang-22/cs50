3c360b15cc979207c6afa31664dce49292c6bcd6
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-14 09:21:26 +0000

    Wed, Dec 14, 2022, 9:21 AM +00:00

--- python/numb3rs/test_numb3rs.py
+++ python/numb3rs/test_numb3rs.py
@@ -1,3 +1,9 @@
 from numb3rs import validate
 
-def 
+def test_numbers():
+    assert validate("255.0.0.1") == True
+    assert validate("512.512.512.512") == False
+    assert validate("127.0.0.-1") == False
+
+def test_not_number():
+    assert validate("cat") == False
