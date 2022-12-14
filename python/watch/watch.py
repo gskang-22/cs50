9e5404d79a3b8d7cf0092264c32ad5111dfdb765
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-14 10:04:54 +0000

    Wed, Dec 14, 2022, 10:04 AM +00:00

--- python/watch/watch.py
+++ python/watch/watch.py
@@ -8,7 +8,7 @@ def main():
 
 def parse(s):
     url = re.search(r'^.*src="(https?://(www\.)?youtube\.com/embed/[\w]+).*', s)
-    shorten = re.sub("")
+    shorten = re.sub("", "")
 
 
 if __name__ == "__main__":
