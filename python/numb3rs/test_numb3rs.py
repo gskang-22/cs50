50ae193634c38f810dbbec6b12ef6e72feac3fc2
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-14 09:08:34 +0000

    Wed, Dec 14, 2022, 9:08 AM +00:00

--- python/numb3rs/test_numb3rs.py
+++ python/numb3rs/test_numb3rs.py
@@ -8,13 +8,19 @@ def main():
 
 def validate(ip):
     a, b, c, d = ip.split(".")
-    if check(int(50)) and check(int(50)) and check(int(50)) and check(int(50)):
+    if check(50) and check(50) and check(50) and check(50):
         return True
     else:
         return False
 
 
-
+def check(n):
+    if int(n) < 0:
+        return False
+    elif int(n) > 255:
+        return False
+    else:
+        return True
 
 
 if __name__ == "__main__":
