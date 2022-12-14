f50d0425bc62927cf84f1b9f7c0036840d0c013f
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-14 10:43:59 +0000

    Wed, Dec 14, 2022, 10:43 AM +00:00

--- python/working/working.py
+++ python/working/working.py
@@ -8,7 +8,10 @@ def main():
 
 def convert(s):
     try:
-        time = re.search("^(0-9:)+[a-zA-Z ]+(0-9:).*$")
+        group = re.search("^([0-9:]+) ([A-Z]+) to ([0-9:]+) ([A-Z]+)$", s.strip())
+        time1, at1, time2, at2 = group.groups()
+        if at1 == "AM":
+            time1 = 
 
 
 ...
