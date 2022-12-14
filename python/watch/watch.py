8598524226a5cafb2eafa63702c54e663428cfc5
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-14 10:13:56 +0000

    Wed, Dec 14, 2022, 10:13 AM +00:00

--- python/watch/watch.py
+++ python/watch/watch.py
@@ -7,8 +7,11 @@ def main():
 
 
 def parse(s):
-    url = re.search(r'^.*src="https?://(?:www\.)?youtube\.com/embed/([\w]+).*', s)
-    return "https://youtu.be/" + url.group(1)
+    try:
+        url = re.search(r'^.*src="https?://(?:www\.)?youtube\.com/embed/([\w]+).*', s)
+        return "https://youtu.be/" + url.group(1)
+    except:
+        return None
 
 
 if __name__ == "__main__":
