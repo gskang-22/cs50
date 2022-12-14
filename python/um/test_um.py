ab659ae5129d8f6f114bfae9d21c0056bdc50dc8
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-14 19:40:14 +0000

    Wed, Dec 14, 2022, 7:40 PM +00:00

---
+++ python/um/test_um.py
@@ -0,0 +1,11 @@
+from um import count
+
+def test_1():
+    assert count("um") == 1
+    assert count("Um") == 1
+    assert count("um?") == 1
+
+def test_2():
+    assert count("Um, thanks, um...") == 2
+
+def test_
