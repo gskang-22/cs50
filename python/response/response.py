7f9cab53d93e9e3f36edb36b0a906959eef59132
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-15 17:48:05 +0000

    Thu, Dec 15, 2022, 5:48 PM +00:00

--- python/response/response.py
+++ python/response/response.py
@@ -1,8 +1,13 @@
+from validator_collection import validators
+
 def main():
     print(validate(input("What's your email address? ")))
 
 def validate(s):
-
+    if is_email(s):
+        return "Valid"
+    else:
+        return "Invalid"
 
 if __name__ == "__main__":
     main()
