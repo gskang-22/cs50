74418ea695b8233ca5607d358180bbd4e20a4802
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-17 09:27:56 +0000

    Sat, Dec 17, 2022, 9:27 AM +00:00

--- python/seasons/test_seasons.py
+++ python/seasons/test_seasons.py
@@ -1,7 +1,8 @@
 from seasons import convert
+import pytest
 
 def test_convert():
-    assert convert("2022-12-17") == "Five hundred twenty-five thousand, six hundred minutes"
+    assert convert("2021-12-17") == "Five hundred twenty-five thousand, six hundred minutes"
 
     with pytest.raises(ValueError):
         convert("January 1, 1999")
