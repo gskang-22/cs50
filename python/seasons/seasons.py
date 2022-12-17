ea30956f0d233749a67624c2b2d5b8aff2023d78
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-17 08:44:59 +0000

    Sat, Dec 17, 2022, 8:44 AM +00:00

--- python/seasons/seasons.py
+++ python/seasons/seasons.py
@@ -1,11 +1,20 @@
 from datetime import date
+import inflect
 
 
 def main():
-    ...
+    print(convert(input("Date of Birth: ")))
 
 
-...
+
+def convert(s):
+    date_today = date.today()
+    try:
+        a, b, c = s.strip().split("-")
+        if not isdigit(a) and not isdigit(b) and not isdigit(c):
+            raise ValueError
+
+        if 
 
 
 if __name__ == "__main__":
