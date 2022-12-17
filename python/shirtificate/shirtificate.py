461e81ab09389398584784450d384219b961a2c4
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-17 20:33:50 +0000

    Sat, Dec 17, 2022, 8:33 PM +00:00

--- python/shirtificate/shirtificate.py
+++ python/shirtificate/shirtificate.py
@@ -4,7 +4,7 @@ def main():
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     pdf.set_font("helvetica", "B", 16)
-    
+    pdf.cell(60, 10, 'CS50 Shirtificate', align='C')
 
 if __name__ == "__main__":
     main()
