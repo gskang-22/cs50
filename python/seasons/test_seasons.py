8e3fba8487228c3368ca7ec50d4ed461bdb8150c
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-17 09:22:56 +0000

    Sat, Dec 17, 2022, 9:22 AM +00:00

--- python/seasons/test_seasons.py
+++ python/seasons/test_seasons.py
@@ -2,4 +2,6 @@ from seasons import convert
 
 def test_convert():
     assert convert("2022-12-17") == "Five hundred twenty-five thousand, six hundred minutes"
-    assert convert("") 
+
+    with pytest.raises(ValueError):
+        convert("January 1, 1999")
