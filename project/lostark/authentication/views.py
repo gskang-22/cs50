d9470764ca4f436d927a1ed44ca3209b7476de7d
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-26 09:52:53 +0000

    Mon, Dec 26, 2022, 9:52 AM +00:00

--- project/lostark/authentication/views.py
+++ project/lostark/authentication/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
 
 # Create your views here.
 
-class RegistrationView(View):
-    def get(self, request):
-        return render(request, 'authentication/register.html')
+class UsernameView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if str(username).isalnum():
+            return
 
 class RegistrationView(View):
     def get(self, request):
