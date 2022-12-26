cadc071d934c8db86e0c2b2513087d370f65ae11
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-26 10:38:41 +0000

    Mon, Dec 26, 2022, 10:38 AM +00:00

--- project/lostark/lostark/static/js/register.js
+++ project/lostark/lostark/static/js/register.js
@@ -1,5 +1,13 @@
-const usernameField.querySelector('#usernameField');
+const usernameField = document.querySelector('#usernameField');
 
-usernameField.addEventListener('keyup', () => {
+usernameField.addEventListener('keyup', (event) => {
     console.log("test");
+
+    const usernameVal = event.target.value;
+    console.log('usernameVal', usernameVal)
+
+    fetch("/authentication/validate-username", {
+        
+    });
+    .then(response)
 });
