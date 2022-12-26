c8e4990873bf70284155826214878e2062c4ce77
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-26 08:38:53 +0000

    Mon, Dec 26, 2022, 8:38 AM +00:00

--- project/lostark/expenses/urls.py
+++ project/lostark/expenses/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="expenses"),
-    path('add_expense', views.add_expense, name="add-expenses")
+    path('add-expense', views.add_expense, name="add-expenses")
 ]
