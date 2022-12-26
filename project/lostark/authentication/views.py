e6972ea5204554ff3c7bdf012c77a6a997962460
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-26 08:57:28 +0000

    Mon, Dec 26, 2022, 8:57 AM +00:00

--- project/lostark/authentication/views.py
+++ project/lostark/authentication/views.py
@@ -4,5 +4,5 @@ from django.views import View
 # Create your views here.
 
 class RegistrationView(View):
-    def get(self.request):
+    def get(self, request):
         return render(request, 'authentication/register.html')
