89d0a5bf990a3fd521646355ea27b353694a561b
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-26 08:34:49 +0000

    Mon, Dec 26, 2022, 8:34 AM +00:00

--- project/lostark/expenses/urls.py
+++ project/lostark/expenses/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="expenses")
+    path('', views.index, name="expenses"),
+    path('add_expense', views.add_expense, name="add-expenses")
 ]
