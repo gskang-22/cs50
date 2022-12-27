3289f9ddaabd2553c782aad4fe198502936ab321
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-27 19:02:17 +0000

    Tue, Dec 27, 2022, 7:02 PM +00:00

--- project/lostark/authentication/views.py
+++ project/lostark/authentication/views.py
@@ -39,4 +39,17 @@ class RegistrationView(View):
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
+
+        if not User.objects.filter(email=email).exists():
+            if not User.objects.filter(username=username).exists():
+                if len(password) < 6:
+                    messages.error(request, 'aPassword too short')
+                    return render(request, 'authentication/register.html')
+
+                user = User.objects.create_user(username=usename, email=email)
+                user.set_password(password)
+                user.save()
+                messages.error(request, 'Account successfully created')
+                return render(request, 'authentication/register.html')
+                
         return render(request, 'authentication/register.html')
