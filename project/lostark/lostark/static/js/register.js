3d4443b8699980b037c3d9766faac0649612fc8e
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-27 10:20:12 +0000

    Tue, Dec 27, 2022, 10:20 AM +00:00

--- project/lostark/lostark/static/js/register.js
+++ project/lostark/lostark/static/js/register.js
@@ -3,7 +3,27 @@ const feedbackArea = document.querySelector(".invalid-feedback");
 const emailField = document.querySelector("#emailField";)
 
 emailField.addEventListener('keyup', (event) => {
-    
+    const emailVal = event.target.value;
+    console.log('usernameVal', usernameVal)
+
+    usernameField.classList.remove("is-invalid");
+    feedbackArea.style.display = 'none';
+
+    if (usernameVal.length > 0) {
+        fetch("/authentication/validate-username", {
+            body: JSON.stringify({username: usernameVal}),
+            method: "POST",
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("data", data);
+            if (data.username_error) {
+                usernameField.classList.add("is-invalid");
+                feedbackArea.style.display = 'block';
+                feedbackArea.innerHTML = `<p>${data.username_error}</p>`
+            }
+        });
+    }
 })
 
 
