ac239dbb263469e3f2cbadeb69c215f38a111cd8
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-27 12:52:43 +0000

    Tue, Dec 27, 2022, 12:52 PM +00:00

--- project/lostark/lostark/static/js/register.js
+++ project/lostark/lostark/static/js/register.js
@@ -5,13 +5,15 @@ const emailFeedbackArea = document.querySelector(".emailFeedbackArea");
 const usernameSuccessOutput = document.querySelector(".usernameSuccessOutput");
 const emailSuccessOutput = document.querySelector(".emailSuccessOutput");
 const showPasswordToggle = document.querySelector(".showPasswordToggle");
-const passwordField = document.querySelector(".passwordField");
+const passwordField = document.querySelector("#passwordField");
 
 const handleToggleInput = (e) => {
     if (showPasswordToggle.textContent == 'SHOW') {
         showPasswordToggle.textContent = 'HIDE';
+        passwordField.setAttribute("type", "text");
     } else {
         showPasswordToggle.textContent = 'SHOW';
+        passwordField.setAttribute("type", "password");
     }
 };
 showPasswordToggle.addEventListener("click", handleToggleInput);
