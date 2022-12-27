21c086e2c654be760729071fa4a32e78891202cc
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-27 18:18:45 +0000

    Tue, Dec 27, 2022, 6:18 PM +00:00

--- project/lostark/authentication/views.py
+++ project/lostark/authentication/views.py
@@ -4,6 +4,7 @@ import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from validate_email import validate_email
+from django.contrib import messages
 
 # Create your views here.
 
@@ -31,4 +32,8 @@ class UsernameValidationView(View):
 
 class RegistrationView(View):
     def get(self, request):
+        return render(request, 'authentication/register.html')
+
+    def post(self, request):
+        message.success('Success whatsapp')
         return render(request, 'authentication/register.html')
