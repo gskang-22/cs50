1499f3957f036f0787c2b3c4753955eac195e394
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-28 12:17:56 +0000

    Wed, Dec 28, 2022, 12:17 PM +00:00

--- project/lostark/authentication/urls.py
+++ project/lostark/authentication/urls.py
@@ -1,4 +1,4 @@
-from .views import RegistrationView, UsernameValidationView, EmailValidationView, LoginView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView, LoginView, LogoutView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
@@ -6,5 +6,7 @@ urlpatterns = [
     path('register', RegistrationView.as_view(), name="register"),
     path('validate-username',  csrf_exempt(UsernameValidationView.as_view()), name="validate-username"),
     path('validate-email', csrf_exempt(EmailValidationView.as_view()), name='validate-email'),
-    path('login', LoginView.as_view(), name="login")
+    path('login', LoginView.as_view(), name="login"),
+    path('logout', LogoutView.as_view(), name="logout"),
+
 ]
