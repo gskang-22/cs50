2c009e402be68661942f6c4d31abf7e6bb1795d8
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-28 12:26:02 +0000

    Wed, Dec 28, 2022, 12:26 PM +00:00

--- project/lostark/expenses/views.py
+++ project/lostark/expenses/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required(login_url='/authentication.login')
+@login_required(login_url='/authentication/login')
 def index(request):
     return render(request, '../templates/expenses/index.html')
 
