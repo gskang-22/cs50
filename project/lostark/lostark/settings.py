5ef8cb46fc08d107a40f3912e09127fbe13b7585
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-28 11:45:28 +0000

    Wed, Dec 28, 2022, 11:45 AM +00:00

--- project/lostark/lostark/settings.py
+++ project/lostark/lostark/settings.py
@@ -137,3 +137,4 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
