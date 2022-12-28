ec152fc7d78a52cdf7f0dda64cdcd39955be0edd
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-28 20:15:12 +0000

    Wed, Dec 28, 2022, 8:15 PM +00:00

--- project/lostark/expenses/models.py
+++ project/lostark/expenses/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Expense(models.Model):
+    amount = models.FloatField()
+    date = models.DateField(default=now)
+    description = models.TextField()
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    catergory = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.category
+
+    # for sorting data according to date in descending order
+    class Meta:
+        ordering: ['-date']
+
+class Category(models.Model):
+    name = models.CharField(max_Length = 255)
