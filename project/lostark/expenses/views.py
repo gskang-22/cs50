ffecd71023dc7e35fda4325e7dc5e34fa8092e35
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-28 20:43:24 +0000

    Wed, Dec 28, 2022, 8:43 PM +00:00

--- project/lostark/expenses/views.py
+++ project/lostark/expenses/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Category, Expense
 
 # Create your views here.
 
 @login_required(login_url='/authentication/login')
 def index(request):
+    categories = Category.objects.all()
     return render(request, '../templates/expenses/index.html')
 
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'expenses/add_expense.html', context)
