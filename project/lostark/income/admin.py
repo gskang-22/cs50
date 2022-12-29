00fff97caec406b6a8e4dfa99a14e9a813001119
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-29 21:07:32 +0000

    Thu, Dec 29, 2022, 9:07 PM +00:00

--- project/lostark/income/admin.py
+++ project/lostark/income/admin.py
@@ -3,7 +3,8 @@ from .models import Income, Source
 
 # customize admin website
 class IncomeAdmin(admin.ModelAdmin):
-    lsit_display = ('owner', 'amount', 'source', 'description', 'date')
+    list_display = ('owner', 'amount', 'source', 'description', 'date')
+    search_field = ('owner', 'amount', 'source', 'description', 'date')
 
 # Register your models here.
 
