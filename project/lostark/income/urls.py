4a7d596306181f3b3d135ea6aec24aca94710847
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-29 20:23:37 +0000

    Thu, Dec 29, 2022, 8:23 PM +00:00

--- project/lostark/income/urls.py
+++ project/lostark/income/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('add-income', views.add_income, name="add-income"),
     path('edit-income/<int:id>', views.edit_income, name="edit-income"),
     path('delete-income/<int:id>', views.delete_income, name="delete-income"),
-    path('search-income', csrf_exempt(views.search_income), name="search-income"),
+    path('search-income', csrf_exempt(views.search_income), name="search-income")
 ]
