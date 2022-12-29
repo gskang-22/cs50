30706d3053735a25855654b818404b97b5274328
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-29 19:28:48 +0000

    Thu, Dec 29, 2022, 7:28 PM +00:00

--- project/lostark/income/models.py
+++ project/lostark/income/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Income(models.Model):
+    amount = models.FloatField()
+    date = models.DateField(default=now)
+    description = models.TextField()
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.category
+
+    # for sorting data according to date in descending order
+    class Meta:
+        ordering = ['-date']
+
+class Category(models.Model):
+    name = models.CharField(max_length = 255)
+
+    # defining how this model should be called in plural form (ie categorys vs categories)
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
