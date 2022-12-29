5fb2a733f0acc5bf86a31c529ca586de953e0f93
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-29 17:49:27 +0000

    Thu, Dec 29, 2022, 5:49 PM +00:00

--- project/lostark/lostark/static/js/searchExpenses.js
+++ project/lostark/lostark/static/js/searchExpenses.js
@@ -4,6 +4,21 @@ searchField.addEventListener('keyup', (e) =>{
     const searchValue = e.target.value;
 
     if (searchValue.length > 0) {
-        console.log("asdf");
+        fetch("/authentication/validate-email", {
+            body: JSON.stringify({email: emailVal}),
+            method: "POST",
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            emailSuccessOutput.style.display = 'none';
+            if (data.email_error) {
+                submitBtn.disabled = true;
+                emailField.classList.add("is-invalid");
+                emailFeedbackArea.style.display = 'block';
+                emailFeedbackArea.innerHTML = `<p>${data.email_error}</p>`
+            } else if (feedbackArea.style.display == 'none') {
+                submitBtn.removeAttribute('disabled');
+            };
+        });
     }
 });
