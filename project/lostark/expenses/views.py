b0265ab0a7ac15d2bb083eb63402760b62c045f5
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-29 15:41:01 +0000

    Thu, Dec 29, 2022, 3:41 PM +00:00

--- project/lostark/expenses/views.py
+++ project/lostark/expenses/views.py
@@ -95,10 +95,12 @@ def delete_expense(request, id):
 def search_expenses(request):
     if request.method == 'POST':
         search_str = json.loads(request.body).get('searchText')
+
         expenses = Expense.objects.filter(
             amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
             date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
             description__icontains=search_str, owner=request.user) | Expense.objects.filter(
             category__icontains=search_str, owner=request.user)
+
         data = expenses.values()
         return JsonResponse(list(data), safe=False)
