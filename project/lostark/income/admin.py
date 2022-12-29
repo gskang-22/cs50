f9a1875abd623242cb51d873164ba33c3e28ef1b
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-29 21:06:04 +0000

    Thu, Dec 29, 2022, 9:06 PM +00:00

--- project/lostark/income/admin.py
+++ project/lostark/income/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Income, Source
 
+# customize admin website
+class IncomeAdmin(admin.ModelAdmin):
+    lsit_display = ('owner', 'amount', 'source', 'description', 'date')
+
 # Register your models here.
 
-admin.site.register(Income)
+admin.site.register(Income, IncomeAdmin)
 admin.site.register(Source)
