29dc97267aa3609908a0236585152c6123361962
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-29 17:57:18 +0000

    Thu, Dec 29, 2022, 5:57 PM +00:00

--- project/lostark/expenses/urls.py
+++ project/lostark/expenses/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', views.index, name="expenses"),
     path('add-expenses', views.add_expense, name="add-expenses"),
     path('edit-expenses/<int:id>', views.edit_expense, name="edit-expenses"),
     path('delete-expenses/<int:id>', views.delete_expense, name="delete-expenses"),
-    path('search-expenses', views.search_expenses, name="search-expenses"),
+    path('search-expenses', csrf_exempt(views.search_expenses), name="search-expenses"),
 ]
