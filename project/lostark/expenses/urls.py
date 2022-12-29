67e6ca63b892999a93bcd706569b19292777e470
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-29 17:55:52 +0000

    Thu, Dec 29, 2022, 5:55 PM +00:00

--- project/lostark/expenses/urls.py
+++ project/lostark/expenses/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name="expenses"),
     path('add-expenses', views.add_expense, name="add-expenses"),
     path('edit-expenses/<int:id>', views.edit_expense, name="edit-expenses"),
-    path('delete-expenses/<int:id>', views.delete_expense, name="delete-expenses")
+    path('delete-expenses/<int:id>', views.delete_expense, name="delete-expenses"),
+    path('search-expenses', views.search_expenses, name="search-expenses"),
 ]
--- project/lostark/lostark/static/js/searchExpenses.js
+++ project/lostark/lostark/static/js/searchExpenses.js
@@ -3,22 +3,14 @@ const searchField = document.querySelector('#searchField');
 searchField.addEventListener('keyup', (e) =>{
     const searchValue = e.target.value;
 
-    if (searchValue.length > 0) {
+    if (searchValue.trim().length > 0) {
         fetch("/expenses/search-expenses", {
-            body: JSON.stringify({email: emailVal}),
+            body: JSON.stringify({searchText: searchValue}),
             method: "POST",
         })
         .then((res) => res.json())
         .then((data) => {
-            emailSuccessOutput.style.display = 'none';
-            if (data.email_error) {
-                submitBtn.disabled = true;
-                emailField.classList.add("is-invalid");
-                emailFeedbackArea.style.display = 'block';
-                emailFeedbackArea.innerHTML = `<p>${data.email_error}</p>`
-            } else if (feedbackArea.style.display == 'none') {
-                submitBtn.removeAttribute('disabled');
-            };
+            console.log("data", data);
         });
     }
 });
