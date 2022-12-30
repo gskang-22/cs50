74acb9f1897f52256696c2562add090c53229320
gskang123 <118478459+gskang123@users.noreply.github.com>
2022-12-30 18:43:11 +0000

    Fri, Dec 30, 2022, 6:43 PM +00:00

--- project/lostark/lostark/static/js/stats.js
+++ project/lostark/lostark/static/js/stats.js
@@ -42,7 +42,7 @@ const getChartData = () => {
     .then(results => {
         console.log("results", results);
         const expense_category_data = results.expense_category_data;
-        const[data, labels] = [Object.keys(category_data), Object]
+        const [data, labels] = [Object.keys(category_data), Object.values(category_data)]
 
         renderChart([], []);
     })
